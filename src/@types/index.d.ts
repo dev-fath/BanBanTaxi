157dd8ce03756d8051dd305704aa0c75722660b9
dev-fath <ggje33@gmail.com>
2022-01-07 12:12:30 +0900

    네비게이션 및 폰트 라이브러리 추가

---
+++ src/@types/index.d.ts
@@ -0,0 +1,10 @@
+// src/@types/index.d.ts
+interface ITheme {
+  color: {
+    white: string;
+    black: string;
+  };
+  fonts: {
+    normal: string;
+  };
+}
---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { ScrollView, Text, View } from 'react-native';
+
+const HomeScreen = () => {
+  return (
+    <ScrollView>
+      <View>
+        <Text>this is Home Screen</Text>;
+      </View>
+    </ScrollView>
+  );
+};
+
+export default HomeScreen;
